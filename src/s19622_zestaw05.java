0b96ce8a58a508e2eacea4f8abf1dc6562bd7910
JoAn <joannacalinska@gmail.com>
2018-11-10 21:50:12 +0100

    java-ex.3

--- src/s19622_zestaw05.java
+++ src/s19622_zestaw05.java
@@ -1,10 +1,11 @@
+import java.util.Arrays;
 import java.util.Random;
 
 public
     class s19622_zestaw05 {
 
-    public static void main( String[] args ) {
-        cw_02();
+    public static void main(String[] args) {
+        cw_04();
     }
     /*
     Dana jest jednowymiarowa tablica zmiennych typu char wypełniona
@@ -52,14 +53,60 @@ public
         int length = tab.length;
         for (int i = 0; i < tab.length; i++) {
             if (i == 0) {
-                toDisplay = "Wartość: " + tab[i] + " ma sąsiada z lewej: " + tab[length-1] + " oraz sąsiada z prawej: " + tab[i+1];
-            } else if (i == length-1) {
-                toDisplay = "Wartość: " + tab[i] + " ma sąsiada z lewej: " + tab[i-1] + " oraz sąsiada z prawej: " + tab[0];
+                toDisplay = "Wartość: " + tab[i] + " ma sąsiada z lewej: " + tab[length - 1] + " oraz sąsiada z prawej: " + tab[i + 1];
+            } else if (i == length - 1) {
+                toDisplay = "Wartość: " + tab[i] + " ma sąsiada z lewej: " + tab[i - 1] + " oraz sąsiada z prawej: " + tab[0];
             } else {
-                toDisplay = "Wartość: " + tab[i] + " ma sąsiada z lewej: " + tab[i-1] + " oraz sąsiada z prawej: " + tab[i+1];
+                toDisplay = "Wartość: " + tab[i] + " ma sąsiada z lewej: " + tab[i - 1] + " oraz sąsiada z prawej: " + tab[i + 1];
             }
 
             System.out.println(toDisplay);
         }
     }
+
+    /*
+    Dane są dwie tablice arr1 i arr2 wypełnione losowymi wartościami
+    liczb całkowitych. Utwórz program który:
+    • utworzy nową tablicę zawierającą wszystkie elementy z obu tablic;
+    • utworzy nową tablicę zawierającą elementy występujące w obu tablicach;
+    • utworzy nową tablicę która pomieści wszystkie takie
+    liczby całkowite z przedziału pomiędzy elementem o najmniejszej
+    a największej wartości z obu tablic, które nie znajdują się
+    w tych tablicach.
+     */
+
+
+    static public void cw_03() {
+        Random r = new Random();
+        double randomValue;
+        double[] a = new double[5];
+        double[] b = new double[5];
+
+        for (int i = 0; i < a.length; i++) {
+            randomValue = r.nextDouble() * 100;
+            a[i] = randomValue;
+            System.out.print(a[i] + ", ");
+        }
+        System.out.println();
+
+        for (int i = 0; i < b.length; i++) {
+            randomValue = r.nextDouble() * 100;
+            b[i] = randomValue;
+            System.out.print(b[i] + ", ");
+        }
+
+//        double[] newArr = Arrays.copyOf(a, a.length + b.length);
+//        System.arraycopy(b, 0, newArr, a.length, b.length);
+        System.out.println();
+
+        double[] newArr = new double[10];
+        for (int i = 0; i < a.length; i++) {
+            newArr[i] = a[i];
+        }
+        for (int i = 0; i < b.length; i++) {
+            newArr[a.length + i] = b[i];
+        }
+        System.out.println(Arrays.toString(newArr));
+    }
+
 }
