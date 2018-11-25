fe7bec5ca2cec5d8f65730a52d202dea9796fc6a
JoAn <joannacalinska@gmail.com>
2018-11-25 22:05:13 +0100

    ex.3

--- src/s19622_zestaw07.java
+++ src/s19622_zestaw07.java
@@ -1,8 +1,10 @@
+import java.util.Arrays;
+
 
 public class s19622_zestaw07 {
 
     public static void main(String[] args) {
-        cw_02();
+        cw_03();
     }
 
     /*
@@ -36,7 +38,7 @@ Jaki jest powód uzyskanego rezultatu?
 
     static int countSigns(char letter, char[] arr) {
         int counter = 0;
-        for(int i=0; i<arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             if (arr[i] == letter) {
                 counter++;
             }
@@ -46,10 +48,76 @@ Jaki jest powód uzyskanego rezultatu?
 
     static void cw_02() {
         char[] array = {'a', 'l', 'a', 'm', 'a', 'k', 'o', 't', 'a'};
-        for (int i=0; i<array.length; i++) {
+        for (int i = 0; i < array.length; i++) {
             System.out.println(array[i] + " wystepuje " + countSigns(array[i], array));
         }
     }
 
+    /*
+    Utwórz i wypełnij losowymi wartościami dwie tablice zmiennych typu int.
+    Następnie utwórz metodę przyjmującą obie tablice i dodatkowo wartość typu int
+    jako argumenty. Zadaniem metody będzie:
+• dostarczenie tablicy, której wartościami będą sumy elementów na tych samych
+indeksach, gdy wartość argumentu typu int będzie mniejsza od 0;
+Uwaga - elementy które nie maja pary, powinny zostać pominięte
+• dostarczenie tablicy zawierającej tylko te elementy, które nie mają
+"pary"indeksowej, gdy wartość argumentu typu int będzie więsza od 0
+• dostarczenie elementu pustego gdy tablice są tej samej długości.
+     */
 
+    static int[] myFunction(int[] table1, int[] table2, int number) {
+        int[] longerArray = table1.length > table2.length ? table1 : table2;
+        int[] shorterArray = table1.length < table2.length ? table1 : table2;
+        int[] newArray;
+
+        if (number < 0 && table1.length != table2.length) {
+            newArray = new int[shorterArray.length];
+            for (int i = 0; i < shorterArray.length; i++) {
+                newArray[i] = table1[i] + table2[i];
+            }
+            return newArray;
+        }
+        if (number > 0 && table1.length != table2.length) {
+            newArray = new int[longerArray.length - shorterArray.length];
+            for (int i = 0; i < newArray.length; i++) {
+                newArray[i] = longerArray[shorterArray.length + i];
+            }
+            return newArray;
+        } else {
+            return newArray = new int[0];
+        }
+    }
+
+    static void cw_03 () {
+        int[] array1 = new int[6];
+        int[] array2 = new int[10];
+
+        for (int i = 0; i < array1.length; i++) {
+            array1[i] = (int) (Math.random() * 10);
+        }
+
+        for (int i = 0; i < array2.length; i++) {
+            array2[i] = (int) (Math.random() * 10);
+        }
+
+        System.out.print(Arrays.toString(array1));
+        System.out.println("");
+        System.out.print(Arrays.toString(array2));
+        System.out.println("");
+        System.out.println(Arrays.toString(myFunction(array1, array2, -2)));
+    }
+
+    /*
+    Utwórz i przetestuj metodę rekurencyjną, która stwierdzi
+    czy dostarczone w tablicy zmiennych typu char słowo jest palindromem.
+     */
+
+//    static boolean test(char[] array) {
+//
+//    }
+//
+//    static void cw_04() {
+//        char []myArray = {'a', 'l', 'a'};
+//        System.out.println(test(myArray));
+//    }
 }
